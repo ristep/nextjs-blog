67af2fa43fdae96e7bea8b2bdacf1107f5b92841
Rste Panovski <ristepan@gmail.com>
2023-07-07 00:52:21 +0200

    Vtor como

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,26 +1,8 @@
-import Head from 'next/head';
-import Date from '../../components/date';
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import utilStyles from '../../styles/utils.module.css';
-
-export async function getStaticProps({ params }) {
-   const postData = await getPostData(params.id);
-   return {
-      props: {
-         postData,
-      },
-   };
-}
-
-
-export async function getStaticPaths() {
-   const paths = getAllPostIds();
-   return {
-      paths,
-      fallback: true,
-   };
-}
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
    return (
@@ -36,24 +18,22 @@ export default function Post({ postData }) {
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
          </article>
       </Layout>
-   );
+   )
 }
 
-// export default function Post({ postData }) {
-//    return (
-//       <Layout>
-//          <Head>
-//             <title>
-//                {postData.title}
-//             </title>
-//          </Head>
-//          {postData.title}
-//          <br />
-//          {postData.id}
-//          <br />
-//          <Date dateString={postData.date} />
-//          <br />
-//          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-//       </Layout>
-//    );
-// }
+export async function getStaticPaths() {
+   const paths = getAllPostIds()
+   return {
+      paths,
+      fallback: false
+   }
+}
+
+export async function getStaticProps({ params }) {
+   const postData = await getPostData(params.id)
+   return {
+      props: {
+         postData
+      }
+   }
+}
