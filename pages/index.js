f0d73274039562fb41d4eba56a53654650c6abc8
Rste Panovski <ristepan@gmail.com>
2023-07-08 12:53:01 +0200

    Treti Commit

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,9 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Мано најголемиот комита во тиквешкиот регион!</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (Овој сајт е само за проба )и нема многу врска со Мано Влаинката :)
         </p>
       </section>
 
