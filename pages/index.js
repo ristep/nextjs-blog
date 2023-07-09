9de1766a2637dba23e9de957864c1f1b0610cfe9
Rste Panovski <ristepan@gmail.com>
2023-07-09 20:36:14 +0200

    Main Branch

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ allPostsData }) {
           (Овој сајт е само за проба и нема многу врска со легендарниот Мано Влаинката :)
         </p>
         <p>
-          Ver. 0.3.0a - Mani branch!
+          Ver. 0.3.0b - MainBranch!
         </p>
       </section>
 
