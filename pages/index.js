770b0d3020557af5cf85113099c894e0de7ba2dd
Rste Panovski <ristepan@gmail.com>
2023-07-09 23:15:43 +0200

    Lele aman

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ allPostsData }) {
           (Овој сајт е само за проба и нема многу врска со легендарниот Мано Влаинката :)
         </p>
         <p>
-          Ver. 0.3.0b - MainBranch!
+          Ver. 0.3.1a - developer
         </p>
       </section>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+//** @type {import('tailwindcss').Config} */
 module.exports = {
    content: [
       './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
